4e9772fd1a05795ca5595105d78dbb06e181f8b1
Bhargavi-Savani <76099040+Bhargavi-Savani@users.noreply.github.com>
2022-02-25 09:38:39 +0530

    B

--- Timetable/selection/views.py
+++ Timetable/selection/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponce
 from django.shortcuts import render
 
 # Create your views here.
+
+def selection(request):
+    return render (request, 'selection/selection.html')
