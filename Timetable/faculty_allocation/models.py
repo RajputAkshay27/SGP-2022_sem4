21351be9db65db9264462fd551d579b7fb46780f
Rajput Akshay <72251117+RajputAkshay27@users.noreply.github.com>
2022-04-01 14:38:33 +0530

    A

--- Timetable/faculty_allocation/models.py
+++ Timetable/faculty_allocation/models.py
@@ -1,12 +1,16 @@
+from enum import unique
 from django.db import models
-
+from selection.models import subject,faculty,resource
 # Create your models here.
 
 class faculty_alloc(models.Model):
     semester = models.IntegerField(max_length=2)
-    subcode = models.
-    lec_type = models.CharField(max_length=10)
-    faculty = models.ForeignKey(on_delete=models.CASCADE)
+    subcode = models.ForeignKey(subject.sub_code,on_delete=models.CASCADE)
+    lec_type = models.ForeignKey(resource.room_no,on_delete=models.CASCADE)
+    faculty_n = models.ForeignKey(faculty.fac_code,on_delete=models.CASCADE)
     
     def __str__(self) :
         return self.subcode
+    
+    class Meta:
+        unique_together = (('semester','subcode','faculty_n'))
