e2e104e28fe77dde80466eeb0f70b5b4eb475c15
Bhargavi-Savani <76099040+Bhargavi-Savani@users.noreply.github.com>
2022-04-01 15:56:41 +0530

    B

--- Timetable/selection/admin.py
+++ Timetable/selection/admin.py
@@ -1,3 +1,8 @@
+import re
 from django.contrib import admin
-
+from .models import faculty, subject, resource
 # Register your models here.
+
+admin.site.register(faculty)
+admin.site.register(subject)
+admin.site.register(resource)
